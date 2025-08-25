5b03dfc0a460af5c1be6fde7a3308bc26ef4b7a4
lofianimat-cell <lofianimat@gmail.com>
2025-08-25 16:50:05 +0500

    Create Script.js

---
+++ Script.js
@@ -0,0 +1,6 @@
+// Example: popup when Buy Now is clicked
+document.querySelectorAll("button").forEach(btn => {
+  btn.addEventListener("click", () => {
+    alert("Thanks for shopping with Al-Noor Collection!");
+  });
+});
